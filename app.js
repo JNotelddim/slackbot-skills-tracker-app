13b8871bc77b6ba27f031a7f9e1592e13235de58
JNotelddim <JNotelddim@users.noreply.github.com>
2023-03-08 17:27:40 +0000

    Swap to modules and clean up app.js the slightest bit

--- app.js
+++ app.js
@@ -1,74 +1,18 @@
-// Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
-// const { handleAppHomeOpened } = require('./listener/appHomeOpened.js');
+import { App } from "@slack/bolt";
+import { skillsEntryLogModalView } from "./views/skillsEntryLogModal";
+import { handleAppHomeOpened } from "./listener/appHomeOpened";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
-
 
 // All the room in the world for your code
-// app.event('app_home_opened', handleAppHomeOpened);
+app.event("app_home_opened", handleAppHomeOpened);
 
-app.event('app_home_opened', async ({ event, client, context }) => {
-  try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
-
-      /* the user that opened your app's app home */
-      user_id: event.user,
-
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    });
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
-
-
-app.command('/skills', async ({ ack, payload, context }) => {
+app.command("/skills", async ({ ack, payload, context }) => {
   // Acknowledge the command request
   ack();
-  
 
   try {
     const result = await app.client.views.open({
@@ -76,61 +20,17 @@ app.command('/skills', async ({ ack, payload, context }) => {
       // Pass a valid trigger_id within 3 seconds of receiving it
       trigger_id: payload.trigger_id,
       // View payload
-      view: {
-        type: 'modal',
-        // View identifier
-        callback_id: 'skills_entry_log_modal',
-        title: {
-          type: 'plain_text',
-          text: 'Modal title'
-        },
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Welcome to a modal with _blocks_'
-            },
-            accessory: {
-              type: 'button',
-              text: {
-                type: 'plain_text',
-                text: 'Click me!'
-              },
-              action_id: 'button_abc'
-            }
-          },
-          {
-            type: 'input',
-            block_id: 'test_input',
-            label: {
-              type: 'plain_text',
-              text: 'What are your hopes and dreams?'
-            },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'dreamy_input',
-              multiline: true
-            }
-          }
-        ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
-        }
-      }
+      view: skillsEntryLogModalView,
     });
     console.log(result);
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
   }
 });
-
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
