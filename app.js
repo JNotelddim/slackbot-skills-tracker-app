4a5c67c030cee28166fd7b50d4f282be87c3ac46
JNotelddim <JNotelddim@users.noreply.github.com>
2023-03-14 20:08:54 +0100

    Try using slack docs' "getting started" app code
    https://github.com/slackapi/bolt-js-getting-started-app/blob/main/app.js

--- app.js
+++ app.js
@@ -1,35 +1,52 @@
-import { App } from "@slack/bolt";
-import { skillsEntryLogModalView } from "./views/skillsEntryLogModal";
-import { handleAppHomeOpened } from "./listener/appHomeOpened";
+const { App } = require("@slack/bolt");
 
+/* 
+This sample slack application uses SocketMode
+For the companion getting started setup guide, 
+see: https://slack.dev/bolt-js/tutorial/getting-started 
+*/
+
+// Initializes your app with your bot token and app token
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
 });
 
-// All the room in the world for your code
-app.event("app_home_opened", handleAppHomeOpened);
+// Listens to incoming messages that contain "hello"
+app.message("hello", async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`,
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me",
+          },
+          action_id: "button_click",
+        },
+      },
+    ],
+    text: `Hey there <@${message.user}>!`,
+  });
+});
 
-app.command("/skills", async ({ ack, payload, context }) => {
-  // Acknowledge the command request
-  ack();
-
-  try {
-    const result = await app.client.views.open({
-      token: context.botToken,
-      // Pass a valid trigger_id within 3 seconds of receiving it
-      trigger_id: payload.trigger_id,
-      // View payload
-      view: skillsEntryLogModalView,
-    });
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
+app.action("button_click", async ({ body, ack, say }) => {
+  // Acknowledge the action
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 8000);
+  await app.start(process.env.PORT || 3000);
+
   console.log("⚡️ Bolt app is running!");
 })();
