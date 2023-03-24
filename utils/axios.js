e28fc55fd7255f8db90fc3e47d108e74bf22b231
JNotelddim <JNotelddim@users.noreply.github.com>
2023-03-24 09:53:52 -0700

    Sign each request with a fresh JWT - reduce expiry to 2m from 1h

--- utils/axios.js
+++ utils/axios.js
@@ -1,27 +1,54 @@
 const { default: axios } = require("axios");
 const jwt = require("jsonwebtoken");
 
-const getJWT = () => {
+/**
+ * To ensure only requests from this app are being accepted by the API,
+ * we're signing all of them with a Json Web Token using a symmetric / secret-key
+ * algorithm.
+ */
+const getJWT = (body = {}) => {
   const secret = process.env.BOLT_KEY;
 
-  const token = jwt.sign({ foo: "bar" }, secret, {
+  const token = jwt.sign(body, secret, {
     algorithm: "HS256",
-    expiresIn: "1h",
+    expiresIn: "2m",
     issuer: "bolt-app",
   });
 
   return token;
 };
 
+/**
+ * To ensure all the requests are consistently pointed at the API,
+ * we configure this axios instance, and use the instance throughout the app.
+ */
 const axiosInstance = axios.create({
   baseURL: process.env.API_URL,
   headers: {
-    // TODO: make jwt renew for each request.
-    Authorization: `Bearer ${getJWT()}`,
     "Content-Type": "application/json",
   },
   timeout: 1000,
 });
+
+/**
+ * Need to ensure the requests are sent with valid tokens.
+ * If I only sign the token on the initialization of the axiosInstance, the
+ * token will inevitably expire and all requests to the API will fail.
+ * So, with this interceptor, I can add an authorization header with a fresh JWT
+ * on each request.
+ */
+axiosInstance.interceptors.request.use(
+  function (config) {
+    config.headers.Authorization = `Bearer ${getJWT(config.data)}`;
+    return config;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    console.log("Axios Error", error);
+    return Promise.reject(error);
+  }
+);
 
 module.exports = {
   axios: axiosInstance,
